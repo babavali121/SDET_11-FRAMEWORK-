516908723e97ea8e65ed76eebc4d5df3b66ea50d
HP <HP@DESKTOP-RBGISBH>
2021-03-15 13:07:28 +0530

    Git Sample Script

---
+++ sdet11_jenkins/src/test/java/com/crm/vtiger/leads/Createleads.java
@@ -0,0 +1,21 @@
+package com.crm.vtiger.leads;
+
+import org.testng.annotations.Test;
+
+public class Createleads {
+	@Test
+	public void test1(){
+		System.out.println("create 1 lead");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("create 2nd lead");
+	}
+	@Test
+	public void test3()
+	{
+		System.out.println("create 3rd lead");
+	}
+
+}
