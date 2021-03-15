c1c71e98eb9ed3b16cac169af12eeb4deab4fe96
HP <HP@DESKTOP-RBGISBH>
2021-03-15 13:37:41 +0530

    newly added sample script

---
+++ sdet11_jenkins/src/test/java/com/crm/vtiger/leads/Sample01.java
@@ -0,0 +1,12 @@
+package com.crm.vtiger.leads;
+
+import org.testng.annotations.Test;
+
+public class Sample01 {
+	@Test
+	public  void test01()
+	{
+		System.out.println("sample script01");
+	}
+
+}
